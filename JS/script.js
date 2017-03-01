38972acccb8e63042cce4eb42cc2d759d10b4adc
Travis Wise <richardtwise@gmail.com>
2017-03-01 14:55:55 -0700

    added js functionality to read more of text. added class and id to p tag

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
+
+
+
+
 $(document).ready(function(){
     $("#myCarousel").carousel();
 
@@ -22,4 +26,30 @@ $(document).ready(function(){
     });
 });
 
-alert("test");
+
+function showMore(id){
+    document.getElementById(id+'Overflow').className='';
+    document.getElementById(id+'MoreLink').className='hidden';
+    document.getElementById(id+'LessLink').className='';
+}
+
+function showLess(id){
+    document.getElementById(id+'Overflow').className='hidden';
+    document.getElementById(id+'MoreLink').className='';
+    document.getElementById(id+'LessLink').className='hidden';
+}
+
+var len = 200;
+var shrinkables = document.getElementsByClassName('shrinkable');
+if (shrinkables.length > 0) {
+    for (var i = 0; i < shrinkables.length; i++){
+        var fullText = shrinkables[i].innerHTML;
+        if(fullText.length > len){
+            var trunc = fullText.substring(0, len).replace(/\w+$/, '');
+            var remainder = "";
+            var id = shrinkables[i].id;
+            remainder = fullText.substring(len, fullText.length);
+            shrinkables[i].innerHTML = '<span>' + trunc + '<span class="hidden" id="' + id + 'Overflow">'+ remainder +'</span></span>&nbsp;<a id="' + id + 'MoreLink" href="#!" onclick="showMore(\''+ id + '\');">Read More</a><a class="hidden" href="#!" id="' + id + 'LessLink" onclick="showLess(\''+ id + '\');">Show Less</a>';
+        };
+    };
+};
